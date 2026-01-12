241b90c05839bd70541d5a5520d733e600b048a6
divkotak007 <divyanshkotak04@gmail.com>
2026-01-12 21:09:10 +0530

    Update all app features and UI

---
+++ src/medication_data.js
@@ -0,0 +1,166 @@
+{
+  "insulins": [
+    {
+      "generic": "insulin aspart",
+      "brands": ["NovoRapid", "Fiasp", "Aspart (Biocon)"],
+      "india_brands": ["Aspart FlexPen", "Insugen Aspart"],
+      "type": "Rapid-acting",
+      "strengths": ["U-100"]
+    },
+    {
+      "generic": "insulin lispro",
+      "brands": ["Humalog", "Admelog"],
+      "india_brands": ["Liprolog", "Insugen Lispro"],
+      "type": "Rapid-acting",
+      "strengths": ["U-100", "U-200"]
+    },
+    {
+      "generic": "insulin glulisine",
+      "brands": ["Apidra"],
+      "india_brands": ["Apidra SoloStar"],
+      "type": "Rapid-acting",
+      "strengths": ["U-100"]
+    },
+    {
+      "generic": "regular insulin",
+      "brands": ["Humulin R", "Novolin R"],
+      "india_brands": ["Insugen R", "Actrapid"],
+      "type": "Short-acting",
+      "strengths": ["U-100"]
+    },
+    {
+      "generic": "insulin NPH",
+      "brands": ["Humulin N", "Novolin N"],
+      "india_brands": ["Insugen N", "Insulatard"],
+      "type": "Intermediate-acting",
+      "strengths": ["U-100"]
+    },
+    {
+      "generic": "insulin glargine",
+      "brands": ["Lantus", "Basaglar", "Toujeo"],
+      "india_brands": ["Basalog", "Glaritus", "Lantus"],
+      "type": "Long-acting",
+      "strengths": ["U-100", "U-300"]
+    },
+    {
+      "generic": "insulin detemir",
+      "brands": ["Levemir"],
+      "india_brands": ["Levemir FlexPen"],
+      "type": "Long-acting",
+      "strengths": ["U-100"]
+    },
+    {
+      "generic": "insulin degludec",
+      "brands": ["Tresiba"],
+      "india_brands": ["Degludec (Novo Nordisk)"],
+      "type": "Ultra-long-acting",
+      "strengths": ["U-100", "U-200"]
+    },
+    {
+      "generic": "premixed insulin",
+      "brands": ["NovoMix 30", "Humalog Mix 25"],
+      "india_brands": ["Insugen 30/70", "Huminsulin 30/70"],
+      "type": "Premixed",
+      "strengths": ["U-100"]
+    }
+  ],
+  "oral_hypoglycemics": [
+    {
+      "class": "Biguanides",
+      "agents": [
+        {
+          "generic": "metformin",
+          "india_brands": ["Glycomet", "Obimet", "Bigomet"],
+          "doses": ["500 mg", "850 mg", "1000 mg"]
+        }
+      ]
+    },
+    {
+      "class": "Sulfonylureas",
+      "agents": [
+        {
+          "generic": "glimepiride",
+          "india_brands": ["Amaryl", "Glimestar", "Glimipack"],
+          "doses": ["1 mg", "2 mg", "4 mg"]
+        },
+        {
+          "generic": "gliclazide",
+          "india_brands": ["Diamicron", "Glizid"],
+          "doses": ["40 mg", "80 mg", "60 mg MR"]
+        }
+      ]
+    },
+    {
+      "class": "DPP-4 Inhibitors",
+      "agents": [
+        {
+          "generic": "teneligliptin",
+          "india_brands": ["Teneza", "Tenova", "Tenglyn"],
+          "doses": ["20 mg"]
+        },
+        {
+          "generic": "vildagliptin",
+          "india_brands": ["Galvus", "Zomelis"],
+          "doses": ["50 mg"]
+        },
+        {
+          "generic": "sitagliptin",
+          "india_brands": ["Januvia", "Istavel"],
+          "doses": ["50 mg", "100 mg"]
+        }
+      ]
+    },
+    {
+      "class": "SGLT2 Inhibitors",
+      "agents": [
+        {
+          "generic": "dapagliflozin",
+          "india_brands": ["Forxiga", "Oxra", "Daparyl"],
+          "doses": ["5 mg", "10 mg"]
+        },
+        {
+          "generic": "empagliflozin",
+          "india_brands": ["Jardiance", "Empaone"],
+          "doses": ["10 mg", "25 mg"]
+        }
+      ]
+    },
+    {
+      "class": "TZDs",
+      "agents": [
+        {
+          "generic": "pioglitazone",
+          "india_brands": ["Actos", "Pioz"],
+          "doses": ["15 mg", "30 mg"]
+        }
+      ]
+    }
+  ],
+  "fixed_combinations": [
+    {
+      "generic": "metformin + glimepiride",
+      "india_brands": ["Glimet", "Glycomet-GP"],
+      "doses": ["500/1 mg", "500/2 mg"]
+    },
+    {
+      "generic": "metformin + teneligliptin",
+      "india_brands": ["Teneza-M", "Tenglyn-M"],
+      "doses": ["500/20 mg"]
+    },
+    {
+      "generic": "metformin + dapagliflozin",
+      "india_brands": ["Xigduo", "Dapanorm-M"],
+      "doses": ["500/10 mg"]
+    },
+    {
+      "generic": "metformin + empagliflozin",
+      "india_brands": ["Jardiance-M", "Empaone-M"],
+      "doses": ["500/10 mg"]
+    },
+    {
+      "generic": "metformin + vildagliptin",
+      "india_brands": ["Galvus-Met", "Zomelis-M"],
+      "doses": ["500/50 mg", "850/50 mg"]
+    }
+  ]
+}
